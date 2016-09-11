7b4de8edf90692c1f3187d57e474f3e2bf1b247a
Darryn Patterson <darryn@whereismytransport.com>
2016-09-11 17:29:31 +0200

    Added the unit test project.

--- src/AspNetCoreFreeGeoIp/GeoIpWrapper.cs
+++ src/AspNetCoreFreeGeoIp/GeoIpWrapper.cs
@@ -1,11 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
 
 namespace AspNetCoreFreeGeoIp
 {
     public class GeoIpWrapper
     {
+        private const string BaseUrl = "https://freegeoip.net";
+
+        public static GeoIpModel GetGeoIp(string remoteIp)
+        {
+            var format = "json";
+            var requestUrl = $"{format}/{remoteIp}";
+            var request = new RestRequest(requestUrl, Method.GET);
+            var client = new RestClient(BaseUrl);
+            var response = client.Execute(request);
+
+            if (response.StatusCode == HttpStatusCode.Forbidden)
+            {
+                throw new WebException($"Request forbidden: {request.Resource}");
+            }
+
+            var json = response.Content;
+            var content = JObject.Parse(json);
+            var model = JsonConvert.DeserializeObject<GeoIpModel>(content.ToString());
+
+            return model;
+        }
     }
 }
---
+++ test/AspNetCoreFreeGeoIp.Tests/UnitTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AspNetCoreFreeGeoIp.Tests
+{
+    public class UnitTests
+    {
+        #region Constructors
+
+        public UnitTests()
+        {
+        }
+
+        #endregion
+
+        [Fact]
+        public void GetCodes_Test()
+        {
+            var remoteIp = "74.125.224.72";
+
+            var result = GeoIpWrapper.GetGeoIp(remoteIp);
+
+            Assert.Equal(result.Ip, remoteIp);
+            Assert.Equal(result.CountryCode, "US");
+        }
+    }
+}
